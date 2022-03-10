0aa468185b459fa616c3f5f0ae6d0754af1ec3c7
jayjayoikeh <45184110+jayjayoikeh@users.noreply.github.com>
2022-03-10 00:42:35 +0000

    Add files via upload

---
+++ firstDatabase.sql
@@ -0,0 +1,6 @@
+create database firstDatabase;
+use firstDatabase;
+create table login(USERNAME varchar(30) not null, GENDER varchar(10) ,
+SIR_NAME varchar(30) ,PASSWRD varchar(20),
+CONFIRMPASSWRD varchar(20) ,ADDRESS varchar(30),
+EMAIL varchar(30) );
